c7d1a6784d83a604a866ee30182e142db1114438
HimchanBae <84004506+HimchanBae@users.noreply.github.com>
2021-10-05 16:15:15 +0900

    Update Quiz6.py

--- Week3/Quiz6.py
+++ Week3/Quiz6.py
@@ -1,6 +1,6 @@
 year = int(input("년도를 입력해주세요 : "))
 
-if ( year%400 == 0 ) or (( year%4 == 0) and ( year%100 != 0)) :
+if ( year % 400 == 0 ) or (( year % 4 == 0) and ( year % 100 != 0)) :
     print("leap year")
 else :
     print("common year")
