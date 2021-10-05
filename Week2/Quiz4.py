13afceabfde102c22bbd5ad75d516532c712a553
HimchanBae <84004506+HimchanBae@users.noreply.github.com>
2021-10-05 14:49:04 +0900

    Update Quiz4.py

--- Week2/Quiz4.py
+++ Week2/Quiz4.py
@@ -4,6 +4,6 @@ weightM = int(input("민수는 몸무게를 입력하거라 : "))
 heightG = int(input("기영이는 키를 입력하거라 : "))
 weightG = int(input("민수는 몸무게를 입력하거라 : "))
 
-bool = heightM > heightG and weightM > weightG
+bool = ( heightM > heightG ) and ( weightM > weightG )
 
 print(bool)
